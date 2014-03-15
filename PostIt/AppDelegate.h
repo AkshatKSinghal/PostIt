2aa6e6f8f23da24fec50038a10a46df61e275ea0
Akshat Singhal <Akshat@Vizury-MacBook-Pro.local>
2014-03-15 16:10:25 +0530

    Initial Commit

---
+++ PostIt/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PostIt
+//
+//  Created by Akshat Singhal on 15/03/14.
+//  Copyright (c) 2014 info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PostIt/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PostIt
+//
+//  Created by Akshat Singhal on 15/03/14.
+//  Copyright (c) 2014 info. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
